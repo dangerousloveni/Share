f0c4f3cef6e986206c779e3a50483181ec7eba95
IKeepMoving <984213164@qq.com>
2017-11-07 18:56:12 +0800

    个人中心

---
+++ app/src/main/java/xx/com/edu/user/fragment/UserCenterAdapter.java
@@ -0,0 +1,70 @@
+package xx.com.edu.user.fragment;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import xx.com.edu.R;
+import xx.com.edu.message.fragment.MessageAdapter;
+
+/**
+ * Created by SwordIng on 2017/11/7.
+ */
+
+public class UserCenterAdapter extends BaseAdapter {
+    private Context mContext;
+    private String[] arrText = new String[]{
+            "资料","钱包","签到"};
+    private int[] arrImages = new int[]{
+            R.drawable.person_usercenter,
+            R.drawable.wallet_usercenter,
+            R.drawable.sign_usercenter
+    };
+
+    public UserCenterAdapter(Context context) {
+        this.mContext=context;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        UserCenterAdapter.ViewHolder viewHolder;
+
+        if(view ==null) {
+            view = View.inflate(mContext, R.layout.mes_item, null);
+            viewHolder=new UserCenterAdapter.ViewHolder();
+            viewHolder.iv_icon= (ImageView) view.findViewById(R.id.mes_image);
+            viewHolder.tv_title= (TextView) view.findViewById(R.id.mes_txt);
+            view.setTag(viewHolder);
+        }
+        else {
+            viewHolder=(UserCenterAdapter.ViewHolder) view.getTag();
+        }
+        viewHolder.iv_icon.setImageResource(arrImages[i]);
+        viewHolder.tv_title.setText(arrText[i]);
+        return view;
+    }
+    static  class ViewHolder{
+
+        ImageView iv_icon;
+        TextView tv_title;
+    }
+
+}
--- app/src/main/java/xx/com/edu/user/fragment/UserFragment.java
+++ app/src/main/java/xx/com/edu/user/fragment/UserFragment.java
@@ -2,6 +2,7 @@ package xx.com.edu.user.fragment;
 
 import android.view.Gravity;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import xx.com.edu.R;
@@ -15,9 +16,15 @@ import xx.com.edu.base.BaseFragment;
  * @updateDes ${TODD}
  */
 public class UserFragment extends BaseFragment {
+    TextView textView;
+    GridView gridView;
     @Override
     public View initView() {
         View view=View.inflate(mContext, R.layout.user_fragment,null);
+        textView=(TextView)view.findViewById(R.id.title_name);
+        textView.setText("个人");
+        gridView=(GridView)view.findViewById(R.id.grid_usercenter);
+        gridView.setAdapter(new UserCenterAdapter(mContext));
         return view;
     }
 
