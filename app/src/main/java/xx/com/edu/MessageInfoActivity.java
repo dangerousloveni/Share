db2ef4241adb86f8bfd180677d7418b77a40a3e9
dangerousloveni <1281541085@qq.com>
2017-11-07 21:22:24 +0800

    Merge branch 'master' of https://github.com/dangerousloveni/Share
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/xx/com/edu/MessageInfoActivity.java
+++ app/src/main/java/xx/com/edu/MessageInfoActivity.java
@@ -2,17 +2,40 @@ package xx.com.edu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import xx.com.edu.home.fragment.bean.MessageBean;
 
 public class MessageInfoActivity extends AppCompatActivity {
 
     private MessageBean messageBean;
+    private ImageView  user_photo;
+    private TextView name;
+    private TextView phone;
+    private  TextView theme;
+    private  TextView content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_info);
         messageBean= (MessageBean) getIntent().getSerializableExtra("messageBean");
+        user_photo= (ImageView) findViewById(R.id.im_userphone);
+        name= (TextView) findViewById(R.id.tv_ssname);
+        phone= (TextView) findViewById(R.id.tv_ssphone);
+        theme=(TextView)findViewById(R.id.tv_stheme);
+        content=(TextView)findViewById(R.id.tv_scontent);
+        if (messageBean!=null)
+        initView(messageBean);
+
+    }
+
+    private void initView(MessageBean messageBean) {
+        user_photo.setImageResource(messageBean.getIamgesID());
+        name.setText(messageBean.getUserName());
+        phone.setText(messageBean.getPhoneNumber());
+        theme.setText(messageBean.getTheme());
+        content.setText(messageBean.getContent());
 
     }
 }
